714e0e655015c10530f819e3455aa0569fed644c
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2018-09-09 23:27:36 +0300

    Added sketeches for ReLU and LogSoftmax layers

---
+++ nn/modules/relu.py
@@ -0,0 +1,14 @@
+from nn.modules import Module
+
+
+class ReLU(Module):
+    """Classic ReLU non-linear layer"""
+
+    def __init__(self):
+        pass
+
+    def forward(self, input_):
+        pass
+
+    def backward(self, grad_output):
+        pass
---
+++ nn/modules/softmax.py
@@ -0,0 +1,14 @@
+from nn.modules import Module
+
+
+class LogSoftmax(Module):
+    """Logarithmic softmax layer"""
+
+    def __init__(self):
+        pass
+
+    def forward(self, input_):
+        pass
+
+    def backward(self, grad_output):
+        pass
